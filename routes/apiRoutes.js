6583d83019c2e46e762d9f5b9556222bd572efe2
Jim Nguyen <jimnguyen220@gmail.com>
2021-03-03 21:54:38 -0600

    post tutor session, app works, need to find how to refresh after delete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,23 +5,40 @@ const fs = require('fs');
 //Routes
 
 module.exports = function(app) {
+    let lastID = database[database.length-1].id;
 
     app.get('/api/notes', function (req, res) {
         res.json(database);
+       
     })
 
-    app.post("/api/notes", function (req, res) {
+    app.post("/api/notes/", function (req, res) {
         const newNote = req.body
+        console.log(newNote)
+        newNote["id"] = lastID + 1;
         database.push(newNote);
-        console.log("Note Saved!")
-        res.end();
-        // fs.writeFile(database, newNote, (err, res) =>{
-        //     if (err) throw err;
-        //     console.log(res);
-        // })
+    
+        fs.writeFile('./db/db.json', JSON.stringify(database), (err, res) =>{
+            if (err) throw err;
+            console.log(res);
+            
+        })
+        
+        res.send({redirect: '/notes'});
     })
+ 
+    app.delete("/api/notes/:id", function (req, res) {
+        const noteId = req.params.id;
+       // filter database data 
+       const result = database.filter(note => note.id != noteId);
+       
 
-    app.delete("api/notes/:id", function (req, res) {
-        const noteId = req.params.id
+        // write filtered data back to database 
+        fs.writeFile('./db/db.json', JSON.stringify(result), (err, res) =>{
+            if (err) throw err;
+            console.log(res);
+        })
+
+       
     })
-}
+};
--- server.js
+++ server.js
@@ -6,8 +6,6 @@
 const express = require("express");
 
 
-
-
 // ==============================================================================
 // EXPRESS CONFIGURATION
 // This sets up the basic properties for our express server
@@ -17,7 +15,7 @@ const express = require("express");
 const app = express();
 
 // Sets an initial port.
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
